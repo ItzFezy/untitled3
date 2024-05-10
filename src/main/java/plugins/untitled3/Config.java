401809f04512d1ac8c86d2499f5da74be7eda85f
itzfezy <itzfezy82@gmail.com>
2024-05-09 19:09:13 -0500

    Initial commit

---
+++ src/main/java/plugins/untitled3/Config.java
@@ -0,0 +1,50 @@
+package plugins.untitled3;
+
+import com.google.common.base.Charsets;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+import static plugins.customconfigapi.ECA.*;
+
+public class Config {
+    private static File file;
+    private static String ConfigName;
+
+
+    private static YamlConfiguration newConfig;
+    public Config(String configName){
+        ConfigName = configName;
+        File config = new File(getPlugin().getDataFolder(), ConfigName);
+        file = config;
+    }
+    public YamlConfiguration getConfig(){
+        File config = new File(getPlugin().getDataFolder(), ConfigName);
+        file = config;
+        return YamlConfiguration.loadConfiguration(config);
+    }
+    public void saveDefaultCustomConfig(){
+        if (!file.exists()){
+            getPlugin().saveResource(ConfigName, false);
+        }
+    }
+    public void reloadCustomConfig() {
+        newConfig = YamlConfiguration.loadConfiguration(file);
+        InputStream defConfigStream = getPlugin().getResource(ConfigName);
+        if (defConfigStream != null) {
+            newConfig.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charsets.UTF_8)));
+        }
+    }
+
+    public void saveCustomConfig() {
+        try {
+            getConfig().save(file);
+        } catch (IOException var2) {
+            getPlugin().getLogger().log(Level.SEVERE, "Could not save config to " + file, var2);
+        }
+    }
+}
---
+++ src/main/java/plugins/untitled3/EasyConfig.java
@@ -0,0 +1,47 @@
+package plugins.untitled3;
+
+import com.google.common.base.Charsets;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+
+import static plugins.customconfigapi.ECA.*;
+
+public class EasyConfig {
+
+        private static File file;
+        private static String ConfigName;
+
+        private static YamlConfiguration newConfig;
+        public static YamlConfiguration getConfig(String configName){
+                ConfigName = configName;
+                File Config = new File(getPlugin().getDataFolder(), ConfigName);
+                file = Config;
+                return YamlConfiguration.loadConfiguration(Config);
+        }
+        public static void saveDefaultCustomConfig(String configName){
+                if (!file.exists()){
+                        getPlugin().saveResource(configName, false);
+                }
+        }
+        public static void reloadCustomConfig(String configName) {
+                newConfig = YamlConfiguration.loadConfiguration(file);
+                InputStream defConfigStream = getPlugin().getResource(configName);
+                if (defConfigStream != null) {
+                        newConfig.setDefaults(YamlConfiguration.loadConfiguration(new InputStreamReader(defConfigStream, Charsets.UTF_8)));
+                }
+        }
+
+        public static void saveCustomConfig(String configName) {
+                try {
+                        getConfig(configName).save(file);
+                } catch (IOException var2) {
+                        getPlugin().getLogger().log(Level.SEVERE, "Could not save config to " + file, var2);
+                }
+
+        }
+}
