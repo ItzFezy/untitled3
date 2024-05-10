bfc3233c506332033e8a98ed831360bd6a7d2f68
ItzFezy <148616668+ItzFezy@users.noreply.github.com>
2024-05-09 19:15:41 -0500

    Update EasyConfig.java

--- src/main/java/plugins/untitled3/EasyConfig.java
+++ src/main/java/plugins/untitled3/EasyConfig.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.logging.Level;
 
-import static plugins.customconfigapi.ECA.*;
+import static plugins.untitled3.ECA.*;
 
 public class EasyConfig {
 
